1a61e132accc7003bc5408c668796c494dc57a00
hrithik20007 <darthvader20137@gmail.com>
2021-04-12 01:57:13 +0530

    37th Commit

--- missingNumber.py
+++ missingNumber.py
@@ -1,9 +1,21 @@
+'''
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+'''
+
+
+
 class Solution(object):
     def missingNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        '''
+        This solution also works but I've gone with the lower solution because it is asked for O(1) space complexity and O(n) runtime complexity.
+
+        
         n=len(nums)
         nums.sort()
         if nums[n-1]==n-1:
@@ -15,5 +27,8 @@ class Solution(object):
             for i in range(0,n):
                 if nums[i]!=k:
                     s=k
+                    break
                 k+=1
             return s
+        '''
+        return sum([i for i in range(len(nums)+1)])-sum(nums)
