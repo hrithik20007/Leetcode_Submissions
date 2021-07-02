a2d3887ff975012ea38aa4f1017c417c2d49e9d3
hrithik20007 <darthvader20137@gmail.com>
2021-07-03 02:36:42 +0530

    200th Solution

---
+++ Easy/removeDuplicates.js
@@ -0,0 +1,56 @@
+/*
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+We repeatedly make duplicate removals on s until we no longer can.
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+Example 1:
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+Example 2:
+Input: s = "azxxzy"
+Output: "ay"
+*/
+
+
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    var arr=[];
+    var i=0;
+    while(i<s.length){
+        arr.push(s[i]);
+        if(i>=1 && arr[arr.length-1]===arr[arr.length-2]){		//When last and second last elements are duplicates, they are popped
+            arr.pop();									
+            arr.pop();
+        }
+        i++;
+    }
+    return arr.join('');
+};
+
+
+
+
+/*
+This works but I wanted a faster solution
+
+
+
+var i=0;
+while(i<s.length){
+    if(s[i]==s[i-1]){
+        s=s.substr(0,i)+s.substr(i+1);				//Deletes the second from the duplicate
+        s=s.substr(0,i-1)+s.substr(i);				//Deletes the first from the duplicate
+        i-=2;										//To maintain i and cover all variables
+    }
+    i++;
+}
+return s;
+*/
