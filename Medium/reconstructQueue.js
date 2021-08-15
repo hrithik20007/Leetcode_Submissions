10bc54183d8329437260773fc62f1780f3e5eea4
hrithik20007 <darthvader20137@gmail.com>
2021-08-15 23:46:40 +0530

    287th Solution

---
+++ Medium/reconstructQueue.js
@@ -0,0 +1,49 @@
+/*
+You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith 
+person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
+Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] 
+is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+
+Example 1:
+Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+Explanation:
+Person 0 has height 5 with no other people taller or the same height in front.
+Person 1 has height 7 with no other people taller or the same height in front.
+Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
+
+Example 2:
+Input: people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+*/
+
+
+
+
+/*
+Logic: First we sort the people according to their height in decreasing order (if same height then we sort according to their ki in ascending order). Then, we 
+just iterate over people and put them into a list 'l', the indexes as per their ki. If there is already an element present at that index, then we push all the 
+elements from that index to their right by one place and add the new element at that index. 
+This logic allows the taller people to occupy the positions first so that the shorter people can be positioned as per their ki easily.
+*/
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function(people) {
+    people.sort((a,b)=>{
+       if(a[0]==b[0])
+           return a[1]-b[1];
+        return b[0]-a[0];
+    });
+    
+    var i,l=[];
+    for(i=0;i<people.length;i++)
+        l.splice(people[i][1],0,people[i]);			//people[i][1] represents the index.
+    
+    return l;
+};
