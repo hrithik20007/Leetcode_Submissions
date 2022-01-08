37d7c1eb0a50fc255818419d094df0563266b3fe
hrithik20007 <darthvader20137@gmail.com>
2022-01-08 18:19:32 +0530

    382nd Solution

---
+++ Easy/lowestCommonAncestor.cpp
@@ -0,0 +1,60 @@
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the 
+lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example 1:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Example 2:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+*/
+
+
+
+
+
+
+/*
+Logic:
+The ancestor will be smaller than p and greater than q (or opposite). It may also be equal to p or q. Our goal is to find 
+that node. We can do it recursively. If we don't find the node in a particular sub-tree, it will return NULL. So, the 
+sub-tree not returning NULL will be returning our required node. 
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return NULL;
+        if((root->val>=p->val && root->val<=q->val) || (root->val<=p->val && root->val>=q->val))
+            return root;
+        
+        TreeNode *left=lowestCommonAncestor(root->left,p,q);
+        TreeNode *right=lowestCommonAncestor(root->right,p,q);
+        
+        if(left!=NULL)
+            return left;
+        if(right!=NULL)
+            return right;
+        return NULL;
+    }
+};
